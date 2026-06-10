57bb9e8278e47c6648149b1449527981805248a9
KashinathPN <kashinathpn21@gmail.com>
2026-06-10 13:23:13 +0530

    Fetch real stock data using yfinance

--- app/main.py
+++ app/main.py
@@ -1,1 +1,12 @@
-print("Stock Market AI Analyzer is starting...")
+import yfinance as yf
+
+stock = yf.Ticker("AAPL")
+
+info = stock.info
+
+print("Company  :", info['longName'])
+print("Price    :", info['currentPrice'])
+print("Currency :", info['currency'])
+print("Market Cap:", info['marketCap'])
+print("52W High :", info['fiftyTwoWeekHigh'])
+print("52W Low  :", info['fiftyTwoWeekLow'])
