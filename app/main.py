21eb3adb46bf398179f317fd8d273b3917965742
KashinathPN <kashinathpn21@gmail.com>
2026-06-11 13:02:40 +0530

    Add 1 year data and moving averages MA20 MA50

--- app/main.py
+++ app/main.py
@@ -6,13 +6,15 @@ stock = yf.Ticker(symbol)
 
 try:
     info = stock.info
-    history = stock.history(period="1mo")
+    history = stock.history(period="1y")
     close_prices = history['Close'].dropna()
 
     highest = round(close_prices.max(), 2)
     lowest  = round(close_prices.min(), 2)
     average = round(close_prices.mean(), 2)
     latest  = round(close_prices.iloc[-1], 2)
+    ma20 = round(close_prices.rolling(window=20).mean().iloc[-1], 2)
+    ma50 = round(close_prices.rolling(window=50).mean().iloc[-1], 2)
     company = info['longName']
 
     print("=" * 40)
@@ -25,6 +27,8 @@ try:
     print("Lowest  :", "$" + str(lowest))
     print("Average :", "$" + str(average))
     print("Latest  :", "$" + str(latest))
+    print("MA20    :", "$" + str(ma20))
+    print("MA50    :", "$" + str(ma50))
     print("=" * 40)
 
 except Exception:
