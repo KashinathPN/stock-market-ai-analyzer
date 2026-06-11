cf08fb34cf1cc66d1a446b6f83e4cdb4886e3aaf
KashinathPN <kashinathpn21@gmail.com>
2026-06-11 12:39:05 +0530

    Add error handling for invalid stock symbols

--- app/main.py
+++ app/main.py
@@ -6,9 +6,14 @@ stock = yf.Ticker(symbol)
 
 info = stock.info
 
-print("Company  :", info['longName'])
-print("Price    :", info['currentPrice'])
-print("Currency :", info['currency'])
-print("Market Cap:", info['marketCap'])
-print("52W High :", info['fiftyTwoWeekHigh'])
-print("52W Low  :", info['fiftyTwoWeekLow'])
+try:
+    print("Company  :", info['longName'])
+    print("Price    :", info['currentPrice'])
+    print("Currency :", info['currency'])
+    print("Market Cap:", info['marketCap'])
+    print("52W High :", info['fiftyTwoWeekHigh'])
+    print("52W Low  :", info['fiftyTwoWeekLow'])
+
+except KeyError:
+    print("Sorry, could not find stock:", symbol)
+    print("Please check the symbol and try again.")
